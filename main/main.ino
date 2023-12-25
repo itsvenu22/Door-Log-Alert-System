18d9f468e0b2587e5c7326c4c14a8d6f535853ad
Venukanthan BS <itsvenu22@gmail.com>
2023-12-26 03:00:50 +0530

    test(TG): Telegram BOT testing

---
+++ main/main.ino
@@ -0,0 +1,66 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+#include "credentials.h"
+
+#define BOT_MESSAGE "ALERT DOOR OPENED"
+const int BUTTON=2;
+
+X509List cert(TELEGRAM_CERTIFICATE_ROOT);
+WiFiClientSecure secured_client;
+UniversalTelegramBot bot(BOT_TOKEN, secured_client);
+
+void setup() {
+  pinMode(BUTTON, INPUT_PULLUP);
+
+  Serial.begin(115200);
+  Serial.println();
+
+ // attempt to connect to Wifi network:
+  Serial.print("Connecting to Wifi SSID ");
+  Serial.print(WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  secured_client.setTrustAnchors(&cert); // Add root certificate for api.telegram.org
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.print("\nWiFi connected. IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("Retrieving time: ");
+  configTime(0, 0, "pool.ntp.org"); // get UTC time via NTP
+  time_t now = time(nullptr);
+  while (now < 24 * 3600)
+  {
+    Serial.print(".");
+    delay(100);
+    now = time(nullptr);
+  }
+  Serial.println(now);
+
+  bot.sendMessage(CHAT_ID, "Bot started up", "");
+}
+void door_open() {
+
+  // Changed to send a Telegram message
+  bot.sendMessage(CHAT_ID, BOT_MESSAGE);
+}
+void door_closed() {
+
+  // Changed to send a Telegram message
+  bot.sendMessage(CHAT_ID, "door closed!");
+}
+void loop() {
+  int button = digitalRead(BUTTON);
+  if (button == LOW)
+  {
+    door_open();
+  }
+  else
+  {
+    door_closed();
+  }
+}
